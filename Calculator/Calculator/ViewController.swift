0df42dcfabc135bc9bebb30d79c8e3bb9d623097
Karthik Kannan <karthikk.314@gmail.com>
2015-04-10 18:31:47 +0530

    - Get text to appear on the display label

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
 
     @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit  = sender.currentTitle!
+        display.text = display.text! + digit
     }
 
 }
