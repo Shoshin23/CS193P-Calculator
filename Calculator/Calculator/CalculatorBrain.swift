27959acf44d70dc810e112dd6a280db4edc3577e
Karthik Kannan <karthikkannan@Kannans-MacBook-Air.local>
2015-05-03 02:44:36 +0530

    - finish CalculatorBrain model.

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -31,5 +31,55 @@ class CalculatorBrain {
         
     }
     
+    private func evaluate(ops : [Op]) -> (result: Double?, remainingOps: [Op]){
+        
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let operation = remainingOps.removeLast()
+            
+            switch operation {
+            case .Operand(let operand):
+                return(operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                 let operandEvalution = evaluate(remainingOps)
+                 if let operand = operandEvalution.result {
+                    return(operation(operand), operandEvalution.remainingOps)
+                }
+                
+            case .BinaryOpeation(_, let operation):
+                let operandEvaluation1 = evaluate(remainingOps)
+                if let operand1 = operandEvaluation1.result {
+                    let operandEvaluation2 = evaluate(operandEvaluation1.remainingOps)
+                    if let operand2 = operandEvaluation2.result{
+                        return(operation(operand1, operand2), operandEvaluation2.remainingOps)
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+        
+        return(nil, ops)
+        
+        
     }
+    
+    func evaluate() -> Double? {
+        return evaluate(opStack).result
+    }
+    
+    private func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    private func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
     
